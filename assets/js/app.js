8703c03668cbcbf9d7e79b881bbcf60116038ed6
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2016-12-21 16:08:04 -0500

    Pour noboru

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,35 @@ class App extends React.Component {
 
   constructor() {
     super();
+
+    this.state = {
+      email : "",
+      password : ""
+    } // state
+
+  } // constructor
+
+  changeEmail(e) {
+    this.setState({ email : e.target.value  })
+  }
+
+  changePassword(e) {
+    this.setState({ password : e.target.value })
   }
 
   render() {
-    return <h1>Hello World</h1>;
-  }
+    return <div>
+      <label>Courriel</label>
+      <input type="email" name="email" value={this.state.email} onChange={this.changeEmail} />
+      <br/>
+      <label>Mot de passe</label>
+      <input type="password" name="password" value={this.state.password} onChange={this.changePassword} />
+      <br/>
+      <button>Envoyer</button>
+    </div>;
+  } // render
 
-}
+} // App
+
 
 export default App
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ module.exports = function() {
   var app = require('express');
   var router = app.Router();
 
-  router.get("/", function(req, res, next) {
+  router.get("*", function(req, res, next) {
 
     res.render('index', {
       title: 'Hello World',
